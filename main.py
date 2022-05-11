2b1f856b12fda59edb3a62af0296656c0275ac4d
mouyuan123 <mouyuancheng2@gmail.com>
2022-05-11 17:30:16 +0800

    hi

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+array = []
+with open("testing.txt", "r", encoding="utf-8") as file:
+    textLine = file.readline()
+    while textLine:
+        splitting = re.findall("[a-zA-Z']+", textLine)
+        array = array + splitting
+        textLine = file.readline()
+print(array)
+print(len(array))
 
+array2 = []
+with open("testing2(positive).txt", "r", encoding="utf-8") as file:
+    textLine = file.readline()
+    while textLine:
+        splitting = re.split(r",\s+", textLine)
+        array2 = array2 + splitting
+        textLine = file.readline()
+print(array2)
+count = 0
+for i in range(0, len(array2)):
+    count += len(array2[i].split())
+print(count)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+array3 = []
+with open("stop-words.txt", "r", encoding="utf-8") as file:
+    textLine = file.readline()
+    while textLine:
+        splitting = textLine.split();
+        array3 = array3 + splitting
+        textLine = file.readline()
+print(array3)
+print(len(array3))
