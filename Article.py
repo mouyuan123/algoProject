8513f78688d00fde9b667e701afb6636aa33a81b
mouyuan123 <mouyuancheng2@gmail.com>
2022-05-13 14:56:06 +0800

    hi

---
+++ Article.py
@@ -0,0 +1,41 @@
+import re
+
+
+class Article:
+    articleCount = 0  # Define the number of articles used for the project
+
+    #  Create instance of article
+    def __init__(self, country, textFile, title):
+        self.country = country
+        self.textFile = textFile
+        self.title = title
+        Article.articleCount += 1
+
+    # To extract all the words from the article and store inside an array
+    # Time Complexity  = O(N) , where N = number of lines in an article
+    def extractWords(self):
+        array = []
+        with open(self.textFile, "r", encoding="utf-8") as file:
+            textLine = file.readline()
+            while textLine:
+                textLine = re.sub(r'http\S+', '', textLine)  # Remove all the URL found inside the articles
+                splitting = re.findall("[a-zA-Z']+", textLine)  # Extracts all the "words only" from the article
+                array = array + splitting
+                textLine = file.readline()
+        unique = Article.removeRedundantWords(self, array)
+        return unique
+
+    # To remove all the repeated words extracted from the article (e.g., no 2 words, "good" occur in the array
+    # Time Complexity = O(N), where N = length of the array
+    def removeRedundantWords(self, array):
+        constArrayWord = []
+        unique = set()
+        for i in range(0, len(array)):
+            if array[i] not in unique:
+                unique.add(array[i])
+                constArrayWord.append(array[i])
+        return constArrayWord
+
+    # Format and print out the article's sequence, country and title
+    def toString(self):
+        print("Article: %d%3s Country: %s%3s \nTitle => %s\n" % (Article.articleCount, " ", self.country, " ", self.title))
--- main.py
+++ main.py
@@ -1,35 +1,34 @@
+import Article  # Import the class we created here to create its instances
 import re
 
-array = []
-with open("SG-1.txt", "r", encoding="utf-8") as file:
-    textLine = file.readline()
-    while textLine:
-        textLine = re.sub(r'http\S+', '', textLine)
-        splitting = re.findall("[a-zA-Z']+", textLine)
-        array = array + splitting
-        textLine = file.readline()
-print(array)
-print(len(array))
+article1 = Article.Article("Singapore", "SG-1.txt", "Getting Singapore in shape: Economic challenges and how to meet them")
+print(article1.extractWords())
+print(len(article1.extractWords()))
+article2 = Article.Article("Singapore", "SG-2.txt", "Economic Development and Social Integration: Singapore’s Evolving Social Compact")
+article1.toString()
+article2.toString()
 
-array2 = []
-with open("Positive.txt", "r", encoding="utf-8") as file:
-    textLine = file.readline()
-    while textLine:
-        splitting = re.split(r",\s+", textLine)
-        array2 = array2 + splitting
-        textLine = file.readline()
-print(array2)
-count = 0
-for i in range(0, len(array2)):
-    count += len(array2[i].split())
-print(count)
 
-array3 = []
-with open("Stop.txt", "r", encoding="utf-8") as file:
-    textLine = file.readline()
-    while textLine:
-        splitting = textLine.split();
-        array3 = array3 + splitting
-        textLine = file.readline()
-print(array3)
-print(len(array3))
+#
+# array2 = []
+# with open("Positive.txt", "r", encoding="utf-8") as file:
+#     textLine = file.readline()
+#     while textLine:
+#         splitting = re.split(r",\s+", textLine)
+#         array2 = array2 + splitting
+#         textLine = file.readline()
+# print(array2)
+# count = 0
+# for i in range(0, len(array2)):
+#     count += len(array2[i].split())
+# print(count)
+#
+# array3 = []
+# with open("Stop.txt", "r", encoding="utf-8") as file:
+#     textLine = file.readline()
+#     while textLine:
+#         splitting = textLine.split();
+#         array3 = array3 + splitting
+#         textLine = file.readline()
+# print(array3)
+# print(len(array3))
