9fbc5147b8d974a9c272aa33d9190e153834ab04
mouyuan123 <mouyuancheng2@gmail.com>
2022-05-26 22:00:56 +0800

    latest

--- AhoCorasick.py
+++ AhoCorasick.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+""" Store the positive, negative and neutral words in a 2D array before searching through the long text (each article) """
 AdjList = []
 
 def process(wordList, text):
@@ -12,10 +13,13 @@ def init_trie(keywords):
     add_keywords(keywords)
     set_fail_transitions()
 
-
+# We will insert the words into a trie before implementing the searching function
 def create_empty_trie():
     """ initalize the root of the trie """
-    AdjList.append({'value': '', 'next_states': [], 'fail_state': 0, 'output': []})
+    # 'next_states are the goto state that check whether the character exists along the trie
+    # 'fail_state' is the failure state that allows the searching backtracks to the longest suffix in the trie
+    # 'output' is the container that stores the complete word at the end of the inserting process
+    AdjList.append({'value': '', 'next_states': [], 'fail_state': 0, 'output': []}) # The empty root works as a failure state
 
 
 def add_keywords(keywords):
@@ -25,6 +29,7 @@ def add_keywords(keywords):
 
 
 def find_next_state(current_state, value):
+    # current_state = 0 represents the root is being accessed (Always insert starting from the root)
     for node in AdjList[current_state]["next_states"]:
         if AdjList[node]["value"] == value:
             return node
@@ -35,8 +40,9 @@ def add_keyword(keyword):
     """ add a keyword to the trie and mark output at the last node """
     current_state = 0
     j = 0
-    keyword = keyword.lower()
-    child = find_next_state(current_state, keyword[j])
+    keyword = keyword.lower() # Convert all the characters to lowercase before program execution
+    child = find_next_state(current_state, keyword[j]) # Check whether the first character exists in the root
+    """ If the first character is linked to the root, we will go down to the trie """
     while child is not None:
         current_state = child
         j = j + 1
@@ -44,20 +50,29 @@ def add_keyword(keyword):
             child = find_next_state(current_state, keyword[j])
         else:
             break
+    """ Insert the words into the trie at the place where it is not found """
     for i in range(j, len(keyword)):
         node = {'value': keyword[i], 'next_states': [], 'fail_state': 0, 'output': []}
+        """ Whenever a new node is added, it is appended to the back of the AdjList """
         AdjList.append(node)
+        """ Link the previous node with the node created using the 'next_states' container """
         AdjList[current_state]["next_states"].append(len(AdjList) - 1)
-        current_state = len(AdjList) - 1
+        current_state = len(AdjList) - 1 # After the link is built, now we move on to the next state
+    """ After we insert a word into the trie completely, the 'output' container of the last character in the word will store the whole word """
+    # E.g., for "he" AdjList[2]["output"] will store "he"
     AdjList[current_state]["output"].append(keyword)
 
 
 def set_fail_transitions():
-    q = deque()
+    q = deque() # Quicker append and pop operations from both end of the container
     child = 0
+    """ Set the failure state for each node """
     for node in AdjList[0]["next_states"]:
         q.append(node)
+        # All the node that connects directly to the root node will have failure state = 0
+        # E.g., "h" has the suffix "h" (excluding itself) and "". Since root has value of "", we will return to the root
         AdjList[node]["fail_state"] = 0
+    """ Here, we use a BFS to build the failure state of each node """
     while q:
         r = q.popleft()
         for child in AdjList[r]["next_states"]:
@@ -73,7 +88,7 @@ def set_fail_transitions():
 
 def get_keywords_found(line):
     """ returns true if line contains any keywords in trie """
-    line = line.lower()
+    line = line.lower() # Lowercase all the characters in the long text (each article)
     current_state = 0
     sentimentLength = 0;
     # keywords_found = []  # (Can use this array to validate whether all the positive words in the article text is found)
